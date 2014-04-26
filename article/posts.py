61caefdf2acd63ad8382774a7db6c944927da7ac
vamdt <liang6688267@gmail.com>
2014-04-27 01:16:15 +0800

    Make it can download html

--- article/posts.py
+++ article/posts.py
@@ -1,24 +1,36 @@
 import re
 import urllib
 import json
+import os
 
 def main():
-    # get_urls('http://www.yinwang.org/')
-    urls = get_urls('index.tmp')
-    urls_json = json.dumps( urls, sort_keys=True, indent=4, separators=(',', ': ') )
-    file = open('urls.tmp', 'w')
-    file.write( urls_json )
-    file.close()
+    urls = get_urls('http://www.yinwang.org/')
+    print urls
+    # urls = get_urls('index.tmp')
+    # urls_json = json.dumps( urls, sort_keys=True, indent=4, separators=(',', ': ') )
+    # file = open('urls.tmp', 'w')
+    # file.write( urls_json )
+    # file.close()
+    dir_str = './download/posts'
+    if not os.path.exists(dir_str):
+        os.mkdir(dir_str)
+    for url in urls:
+        file = open(dir_str + '/' + url['name'] +'.html', 'w')
+        file.write( get_html(url['url']) )
+        file.close
 
 def get_urls(entry):
     urls = []
     f = urllib.urlopen(entry)
     html = f.read()
-    pattern = re.compile(u'<a href="(http://yinwang.org/blog-cn/.*?)>(.*?)</a>', re.DOTALL)
+    pattern = re.compile(u'<a href="(http://yinwang.org/blog-cn/.*?)">(.*?)</a>', re.DOTALL)
     url_wrappers = pattern.findall( html )
     for url_wrapper in url_wrappers:
         urls.append( {'name': url_wrapper[1], 'url': url_wrapper[0]} )
     return urls
+
+def get_html(url):
+    return urllib.urlopen(url).read()
 
 if __name__ == '__main__':
     main()
