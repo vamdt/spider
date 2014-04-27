f464503741ccaccf239d7c68fef3a4d699b36591
vamdt <liang6688267@gmail.com>
2014-04-27 20:42:35 +0800

    Add address entry

--- tickets/ctrip.py
+++ tickets/ctrip.py
@@ -2,7 +2,8 @@
 
 import re, urllib, urllib2, cookielib,json
 
-site_url = 'http://flights.ctrip.com/international/beijing-taipei-bjs-tpe'
+# site_url = 'http://flights.ctrip.com/international/beijing-taipei-bjs-tpe'
+site_url = 'http://flights.ctrip.com/international/beijing-kualalumpur-bjs-kul'
 
 search_url = 'http://flights.ctrip.com/international/GetSubstepSearchResults.aspx?IsJSON=T&queryLogTransNo=3914042718000006946&QueryType=1&cityPairAirline=first&withDirectAirline=T&RdNo=2103213618&sPassType=NOR&ind=742,752,774,754'
 
@@ -14,7 +15,8 @@ def main():
     opener = urllib2.build_opener( urllib2.HTTPCookieProcessor(cookies) )
     urllib2.install_opener(opener)
     req = urllib2.Request(site_url)
-    html = urllib2.urlopen(req)
+    param = urllib.urlencode({'DDatePeriod1': '2014-05-12'})
+    html = urllib2.urlopen(req, param)
     # print html.info()
     for cookie in cookies:
         print cookie.name, cookie.value
@@ -24,7 +26,7 @@ def main():
     raw_str = search_html.read()
     save(raw_str, 'raw_ctrip.txt')
 
-    data = json.loads(raw_str)
+    data = json.loads(raw_str.decode('latin1'))
     save_as_json(data, 'ctrip.txt')
 
     cookies.save('ctrip_cookie.txt')
