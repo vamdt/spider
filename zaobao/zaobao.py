ac22258aef6017084859814e49ab1dd7c529e75c
vamdt <liang6688267@gmail.com>
2017-10-25 20:16:14 +0800

    Completed zaobao fetch

--- zaobao/zaobao.py
+++ zaobao/zaobao.py
@@ -8,23 +8,28 @@ import datetime
 import time
 import email.utils
 
-ZAOBAO_PREFIX = "http://www.zaobao.com"
-list_url = ZAOBAO_PREFIX + "/news/china"
 
 class Zaobao:
-    def main():
+
+    ZAOBAO_PREFIX = "http://www.zaobao.com"
+    list_url = ZAOBAO_PREFIX + "/news/china"
+
+    def __init__():
         set_encoding()
+        self.channel_items = []
+
+    def main(self, page):
         # content = fetch_list_content()
         file = open('zaobao.html', 'r')
         content = file.read()
-        parse_content(content)
+        self.parse_content(content)
 
-    def set_encoding():
+    def set_encoding(self):
         reload(sys)
         sys.setdefaultencoding('utf-8')
 
 
-    def fetch_list_content():
+    def fetch_list_content(self, page):
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3213.3 Safari/537.36',
             'Host': 'www.zaobao.com',
@@ -34,10 +39,12 @@ class Zaobao:
         }
         s = requests.Session()
         s.headers.update(headers)
+        if page > 1:
+            list_url += "?page=" + page
         key_r = s.get(list_url)
         return key_r.content
 
-    def parse_content(content):
+    def parse_content(self, content):
         html = BeautifulSoup(content, 'html.parser')
         #print html.body.find("div", {"class": "container page-wrapper"})
         post_list = html.body.find_all("div", {"class": "post-detail"})
@@ -50,9 +57,20 @@ class Zaobao:
             channel_item.author = item["author"]
             channel_item.pub_date = item["pub_date"]
             channel_items.append(channel_item)
-        generate(channel_items)
 
-    def generate(channel_items):
+    def size(self):
+        return len(self.channel_items)
+
+    def contains_item_by_url(self, url):
+        for channel_item in self.channel_items:
+            if channel_item.link == url:
+                return True
+        return False
+
+    def first_url(self):
+        return self.channel_items[0].link
+
+    def generate(self):
         channel = Channel()
         channel.title = "联合早报网 - 中国新闻"
         channel.link = "http://www.zaobao.com/news/china"
@@ -62,5 +80,5 @@ class Zaobao:
         channel.last_build_date = email.utils.formatdate(time.mktime(now.timetuple()), True)
         channel.image_url = "http://www.zaobao.com/sites/all/themes/zb2016/assets/imgs/zbsg/apple-icon-144x144.png"
         builder = RssBuilder()
-        builder.build(channel, channel_items)
+        builder.build(channel, self.channel_items)
         builder.write("zaobao_rss")
